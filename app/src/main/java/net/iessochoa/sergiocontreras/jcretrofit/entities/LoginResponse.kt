c6ef43eef8af5c63692b8ffb4391a43fa1a22955
mrPancetita <contreras.engineer@outlook.com>
2025-10-25 23:23:39 +0200

    lesson 384 serializedName

--- app/src/main/java/net/iessochoa/sergiocontreras/jcretrofit/entities/LoginResponse.kt
+++ app/src/main/java/net/iessochoa/sergiocontreras/jcretrofit/entities/LoginResponse.kt
@@ -1,17 +1,10 @@
 package net.iessochoa.sergiocontreras.jcretrofit.entities
 
-import com.google.gson.annotations.SerializedName
-
 /**
  * Project: JC Retrofit
  * From: net.iessochoa.sergiocontreras.jcretrofit.entities
  * Created by: Contr
- * On: 25/10/2025 at 22:48
+ * On: 25/10/2025 at 23:22
  * Creado en Settings -> Editor -> File and Code Templates
  */
-
-
-class UserInfo(
-    @SerializedName("email") val email: String,
-    @SerializedName("password ") val pass: String
-)
+class LoginResponse(val token: String)
